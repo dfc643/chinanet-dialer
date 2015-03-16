9bea3473e2cc1ae9670749d52c813b8ce48354e1
北極光.Norckon <dfc643@126.com>
2015-03-16 09:21:48 +0800

    fix some bug

--- pinger.sh
+++ pinger.sh
@@ -8,10 +8,10 @@ WatchDog() {
 	while [ 1 ]
 	do
 		# If cannot pingback dial server, change IP address!
-		if [ $(ping -c10 -W1 118.249.248.1|grep -c '100% packet loss') -eq 1 ]
+		if [ $(ping -c10 -W1 YOUR_PING_SERVER|grep -c '100% packet loss') -eq 1 ]
 		then
 			echo -e "(date '+%D %T') Current IP Address is unavailable, Renew IP Address ..."
-			sshpass -p 'dfc220HAM_' ssh root@192.168.16.254 'killall -SIGUSR2 udhcpc;sleep 1;killall -SIGUSR1 udhcpc;'
+			sshpass -p 'YOUR_PASSWORD' ssh root@YOUR_ROUTER_IP 'killall -SIGUSR2 udhcpc;sleep 1;killall -SIGUSR1 udhcpc;'
 		fi
 	done
 }
